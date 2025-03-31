b36d97f9a2707ef793ac4e9a491c9188fe497a19
dongdongdongk <dhk9309@gmail.com>
2025-03-31 12:45:20 +0900

    방어 시스템 구현현

--- downfall/src/entities/Player.js
+++ downfall/src/entities/Player.js
@@ -28,10 +28,19 @@ class Player extends Phaser.Physics.Arcade.Sprite {
         this.lastDirection = Phaser.Physics.Arcade.FACING_RIGHT;
         this.body.setGravityY(500);
         this.setCollideWorldBounds(true);
+        
         this.cursors = this.scene.input.keyboard.createCursorKeys();
-
+        this.defenseKey = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        
         this.setOrigin(0.5, 1);
         this.health = 100;
+
+        // 방어 관련 변수수
+        this.isDefending = false; // 방어 상태
+        this.facingRight = true;  // 바라보는 방향 (true: 오른쪽, false: 왼쪽)
+        this.defenseDuration = 500; // 방어 지속 시간 (0.5초)
+        this.defenseCooldown = 1000; // 방어 쿨다운 (1초)
+        this.lastDefenseTime = 0; // 마지막 방어 시간
 
     }
 
@@ -39,7 +48,7 @@ class Player extends Phaser.Physics.Arcade.Sprite {
         this.scene.events.on(Phaser.Scenes.Events.UPDATE, this.update, this);
     }
 
-    update() {
+    update(time) {
         if (this.hasBeenHit || this.isSliding || !this.body) {
             return;
         }
@@ -72,11 +81,38 @@ class Player extends Phaser.Physics.Arcade.Sprite {
         if (onFloor) {
             this.jumpCount = 0;
         }
+
+        // 'F' 키로 방어 활성화
+        if (Phaser.Input.Keyboard.JustDown(this.defenseKey) && time > this.lastDefenseTime + this.defenseCooldown) {
+            this.activateDefense(time);
+        }
+
+        // 방어 상태가 끝났는지 확인
+        if (this.isDefending && time > this.defenseEndTime) {
+            this.isDefending = false;
+            this.setTint(0xffffff); // 방어 끝나면 원래 색상으로 (시각적 피드백 제거)
+        }
+    }
+
+    activateDefense(time) {
+        console.log("Player is defending!");
+        this.isDefending = true;
+        this.lastDefenseTime = time;
+        this.defenseEndTime = time + this.defenseDuration;
+        this.setTint(0x00ff00); // 방어 중임을 나타내기 위해 초록색으로 (시각적 피드백)
     }
 
     takesHit(source) {
         if (this.hasBeenHit) {
             return;
+        }
+        if (this.isDefending) {
+            const bounceVelocity = source.bounceVelocity || this.bounceVelocity;
+            this.bounceOff(source, bounceVelocity);
+            // flipX가 false면 오른쪽, true면 왼쪽을 보고 있음
+            const attackFromRight = source.x > this.x;
+            if (!this.flipX && !attackFromRight) return; // 오른쪽을 보고 있고 왼쪽에서 공격
+            if (this.flipX && attackFromRight) return;   // 왼쪽을 보고 있고 오른쪽에서 공격
         }
 
         this.health -= source.damage || 0;
